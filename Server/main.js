49d6a54320e030cf798fc8858da330984c79c9ce
itsDavid08 <128751214+itsDavid08@users.noreply.github.com>
2026-06-09 15:34:43 +0100

    Server run front-end

--- Server/main.js
+++ Server/main.js
@@ -8,6 +8,8 @@ const port = 3000;
 const router = require('./routes/route.js');
 const imagesRouter = require('./routes/images');
 const multer = require('multer');
+const path = require('path');
+const DIST = path.join(__dirname, '../Client/dist');
 
 // Configuração do armazenamento
 const storage = multer.diskStorage({
@@ -23,9 +25,14 @@ const upload = multer({ storage });
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
+app.use(express.static(DIST));
 
 app.use(router);
 app.use(imagesRouter);
+
+app.use((req, res) => {
+  res.sendFile(path.join(DIST, 'index.html'));
+});
 
 // Criação do servidor HTTP e integração com socket.io
 const server = http.createServer(app);
